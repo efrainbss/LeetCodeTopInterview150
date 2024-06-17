48a151bbef32703b40af6f29a37e1380b74da75c
efrainbss <efrainbss@gmail.com>
2024-06-17 14:48:05 -0700

    se agrego deteccion de repetidos en proyecto Remove Duplicates

--- RemoveDuplicates/Solution.cs
+++ RemoveDuplicates/Solution.cs
@@ -1,11 +1,56 @@
 namespace RemoveDuplicates;
 
-public class Solution
+public static class Solution
 {
-    public int RemoveDuplicates(int[] nums)
+    public  static int RemoveDuplicates(ref int[] nums)
     {
+        int result = 0;
+        var dic = new Dictionary<int, int>();
+        
+        Array.Sort(nums);
+        
+        foreach (int val in nums)
+        {
+            var occurrence = FindValueOccurrence(nums, val);
+            if (occurrence <= 1) continue;
+            dic.TryAdd(val, occurrence);
+            
+        }
+        
+        // Console.WriteLine($"Ordered Array: {TestSolution.ArrayToPrint(nums)}");
+        // int aPointer = nums.Length - 1;
+        // int temp = 0;
+        //
+        // for (int i = 0; i < nums.Length; i++)
+        // {
+        //     if (nums[i] == val && nums[aPointer] != val && i < result /*&& !ValueIsInArray(nums, val, result)*/)
+        //     {
+        //         temp = nums[i];
+        //         if (nums[aPointer] == val)
+        //             aPointer--;
+        //         
+        //         nums[i] = nums[aPointer];
+        //         nums[aPointer] = temp;
+        //         aPointer--;
+        //     }
+        // }
+        
+        return result;
+    }
+    
+    private static int FindValueOccurrence(int[] array, int val)
+    {
+        int occurrences = 0; 
+        
+        bool IsVal(int element)
+        {
+            return element == val;
+        }
+        var findValue = Array.FindAll(array, IsVal).ToArray();
+        
+        occurrences = findValue.Length;
 
-        return 0;
+        return occurrences;
     }
     
 }
---
+++ RemoveDuplicates/Test.cs
@@ -0,0 +1,60 @@
+namespace RemoveDuplicates;
+
+
+public static class TestSolution
+{
+    
+    public static void DoTest(int[] nums) {
+        
+        Console.WriteLine($"nums: {ArrayToPrint(nums)}");
+        
+        var k = Solution.RemoveDuplicates(ref nums);
+        
+        Console.WriteLine($"k: {k}");
+        // AssertNotValInExpectedArray(nums, val, k);
+        Console.ReadLine();
+    }
+
+    static void AssertNotValInExpectedArray(int[] nums, int val, int k)
+    {
+        bool ValueInArray = false;
+
+        for(int i = 0; i < k ; i++)
+        {
+           if(val == nums[i])
+           {
+               ValueInArray = true;
+           }
+        }
+
+        if (ValueInArray)
+        {
+            Console.WriteLine($"TEST FAILED: value {val} is present in expected array");
+        }
+        else
+        {
+            Console.WriteLine($"TEST PASSED: value {val} is NOT present in expected array");
+        }
+        Console.WriteLine($"Result array: {ArrayToPrint(nums)}");
+        Console.WriteLine($"Result array shorted to k: {ArrayToPrint(nums, k)}");
+    }
+    
+    public static string ArrayToPrint(int[] arr, int limit = 0)
+    {
+        string strToPrint = "[";
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            strToPrint += $"{arr[i]},";
+            
+            if (limit > 0 && i == limit - 1) 
+                break;
+        }
+
+        strToPrint = strToPrint.Trim(',');
+        strToPrint += "]";
+
+        return strToPrint;
+    }
+}
+
