8c79efa78a6c21e338ab5fbd7b67efcedb9bf4f9
efrainbss <efrainbss@gmail.com>
2024-06-18 09:38:43 -0700

    Problem solved. Not optimized

--- RemoveDuplicates/Solution.cs
+++ RemoveDuplicates/Solution.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace RemoveDuplicates;
 
 public static class Solution
@@ -6,35 +8,33 @@ public static class Solution
     {
         int result = 0;
         var dic = new Dictionary<int, int>();
-        
         Array.Sort(nums);
-        
+        //find ocurrences of each value
         foreach (int val in nums)
         {
             var occurrence = FindValueOccurrence(nums, val);
-            if (occurrence <= 1) continue;
+            // if (occurrence <= 1) continue;
             dic.TryAdd(val, occurrence);
-            
+        }
+        result = dic.Count;
+        //first positions with no repeated values
+        int i = 0;
+        foreach (var (key,value) in dic)
+        {
+            nums[i] = key;
+            i++;
+        }
+        //fill the next positions with the rest values
+        foreach (var (key,value) in dic)
+        {
+            for(int index = 0; index < value -1; index++)
+            {
+                nums[i] = key;
+                i++;
+            }
         }
         
-        // Console.WriteLine($"Ordered Array: {TestSolution.ArrayToPrint(nums)}");
-        // int aPointer = nums.Length - 1;
-        // int temp = 0;
-        //
-        // for (int i = 0; i < nums.Length; i++)
-        // {
-        //     if (nums[i] == val && nums[aPointer] != val && i < result /*&& !ValueIsInArray(nums, val, result)*/)
-        //     {
-        //         temp = nums[i];
-        //         if (nums[aPointer] == val)
-        //             aPointer--;
-        //         
-        //         nums[i] = nums[aPointer];
-        //         nums[aPointer] = temp;
-        //         aPointer--;
-        //     }
-        // }
-        
+        Console.WriteLine($"Ordered Array: {TestSolution.ArrayToPrint(nums)}");
         return result;
     }
     
